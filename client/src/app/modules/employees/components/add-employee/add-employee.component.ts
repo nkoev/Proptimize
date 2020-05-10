3da1e2e0c4704973098ebd37f948eb152a3a0762
nkoev <koev.niki@gmail.com>
2020-05-10 13:02:58 +0300

    "add-employee form added"

--- client/src/app/modules/employees/components/add-employee/add-employee.component.ts
+++ client/src/app/modules/employees/components/add-employee/add-employee.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
 export class AddEmployeeComponent implements OnInit {
+  employeeForm: FormGroup;
+  skillsList = ['Java', 'JavaScript', 'BellyDancing'];
+  managersList = ['Boncho', 'Concho', 'Self-managed'];
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      firstName: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+      lastName: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+      position: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(20),
+        ],
+      ],
+      isManager: false,
+      isAdmin: false,
+      managedBy: null,
+      skills: null,
+    });
   }
 
+  get firstName() {
+    return this.employeeForm.get('firstName');
+  }
+  get lastName() {
+    return this.employeeForm.get('lastName');
+  }
+  get position() {
+    return this.employeeForm.get('position');
+  }
+  get isManager() {
+    return this.employeeForm.get('isManager');
+  }
+  get isAdmin() {
+    return this.employeeForm.get('isAdmin');
+  }
+  get managedBy() {
+    return this.employeeForm.get('managedBy');
+  }
+  get skills() {
+    return this.employeeForm.get('skills');
+  }
+
+  onSubmit(form: FormGroup) {}
 }
--- client/src/app/modules/employees/employees.module.ts
+++ client/src/app/modules/employees/employees.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AllEmployeesComponent } from './all-employees/all-employees.component';
+import { AllEmployeesComponent } from './pages/all-employees/all-employees.component';
 import { DetailEmployeeComponent } from './pages/detail-employee/detail-employee.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AllEmployeesComponent, DetailEmployeeComponent, AddEmployeeComponent],
-  imports: [SharedModule, EmployeesRoutingModule],
+  declarations: [
+    AllEmployeesComponent,
+    DetailEmployeeComponent,
+    AddEmployeeComponent,
+  ],
+  imports: [
+    SharedModule,
+    EmployeesRoutingModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
 })
 export class EmployeesModule {}
