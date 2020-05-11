32584616dbd138a33625548042be159b9709429d
nkoev <koev.niki@gmail.com>
2020-05-11 19:13:31 +0300

    "login-form comp added"

--- client/src/app/modules/login/login-routing.module.ts
+++ client/src/app/modules/login/login-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
 
--- client/src/app/modules/login/login.module.ts
+++ client/src/app/modules/login/login.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { LoginComponent } from './components/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [SharedModule, LoginRoutingModule],
 })
 export class LoginModule {}
