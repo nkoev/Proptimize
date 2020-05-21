f13642f0a90a0340c3b1bf1dadebed6654d96fc3
nkoev <koev.niki@gmail.com>
2020-05-21 18:15:04 +0300

    "google charts preloading"

--- client/src/app/modules/employees/components/orgchart/orgchart.component.ts
+++ client/src/app/modules/employees/components/orgchart/orgchart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DocumentData } from '@google-cloud/firestore';
+import { DocumentData } from '@angular/fire/firestore/interfaces';
 
 @Component({
   selector: 'app-orgchart',
--- client/src/main.ts
+++ client/src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+google.charts.load('current', { packages: ['orgchart'] });
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
