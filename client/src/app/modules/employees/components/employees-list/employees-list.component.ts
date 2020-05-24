cc5b80c17bf75f51294654518b0354ec3e1eb350
nkoev <koev.niki@gmail.com>
2020-05-24 12:08:36 +0300

    "error fixes"

--- client/src/app/modules/employees/components/employees-list/employees-list.component.ts
+++ client/src/app/modules/employees/components/employees-list/employees-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DocumentData } from '@google-cloud/firestore';
-import { EmployeesFilteringFormComponent } from '../employees-filtering-form/employees-filtering-form.component';
 
 @Component({
   selector: 'app-employees-list',
@@ -9,7 +8,6 @@ import { EmployeesFilteringFormComponent } from '../employees-filtering-form/emp
 })
 export class EmployeesListComponent implements OnInit {
   type = 'employees';
-  @Input() filteringFormComp: EmployeesFilteringFormComponent;
   @Input() filteredEmployees: DocumentData[];
   @Input() filteredManagers: DocumentData[];
   @Output() showEmployee = new EventEmitter<DocumentData>();
