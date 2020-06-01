792277652018c8d2b299b0cf3b147d34594fc65f
nkoev <koev.niki@gmail.com>
2020-06-01 18:20:41 +0300

    "user-projects comp added #5"

--- client/src/app/modules/dashboard/components/user-projects/user-projects.component.ts
+++ client/src/app/modules/dashboard/components/user-projects/user-projects.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserDTO } from 'src/app/models/employees/user.dto';
 
 @Component({
   selector: 'app-user-projects',
   templateUrl: './user-projects.component.html',
-  styleUrls: ['./user-projects.component.css']
+  styleUrls: ['./user-projects.component.css'],
 })
 export class UserProjectsComponent implements OnInit {
+  @Input() loggedUser: UserDTO;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
