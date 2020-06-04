1a650318fba2d0dffa5b4d3702ac92ffcf68c098
nkoev <koev.niki@gmail.com>
2020-06-04 19:25:46 +0300

    "login redirect"

--- client/src/app/modules/login/components/login/login.component.ts
+++ client/src/app/modules/login/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/modules/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   today = new Date();
 
-  constructor() {}
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.auth.isLoggedIn$.subscribe((res) => {
+      if (res) {
+        this.router.navigate(['/dashboard']);
+      }
+    });
+  }
 }
